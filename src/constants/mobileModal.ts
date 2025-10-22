bd4d4e8982816fef1e5decc3af8db8068eb249ca
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-10-22 20:32:54 +0100

    refactor: updated moble modal screen

---
+++ src/constants/mobileModal.ts
@@ -0,0 +1,10 @@
+export const MOBILE_MODAL_CONFIG = {
+  title: "Switch Devices",
+  description: "It looks like you're browsing on a mobile device",
+  descriptionSuffix: "This page is best viewed on a laptop or tablet. Please switch to a larger screen to continue.",
+  logoutText: "Log Out",
+  svg: {
+    width: 154,
+    height: 124,
+  },
+} as const;
