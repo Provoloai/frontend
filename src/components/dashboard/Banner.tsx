01e69f7f2009ba2ea24400f3e078d1bd9bf83e72
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-11-14 20:40:28 +0100

    Update Banner.tsx

--- src/components/dashboard/Banner.tsx
+++ src/components/dashboard/Banner.tsx
@@ -13,6 +13,8 @@ export default function Banner() {
 
           <a
             href="https://www.producthunt.com/products/provolo?launch=provolo&utm_source=twitter&utm_medium=social"
+            target="_blank"
+            rel="noopener noreferrer"
             className="flex-none rounded-md bg-black px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-black/85 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
           >
             🔥 Upvote
