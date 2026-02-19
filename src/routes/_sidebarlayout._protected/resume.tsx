47e6d1b2af591f15747ce8e5fc3ca8ed0cbb575a
Mubashir <mubashirnafiu@gmail.com>
2026-02-19 11:31:55 +0100

    protect resume page based on user's subscription

--- src/routes/_sidebarlayout._protected/resume.tsx
+++ src/routes/_sidebarlayout._protected/resume.tsx
@@ -1,10 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Resume } from "../../pages/resume-generator/Resume";
+import useSession from "@/hooks/useSession";
+import { useEffect } from "react";
+import VerifyingAuth from "@/Reusables/VerifyingAuth";
 
 export const Route = createFileRoute("/_sidebarlayout/_protected/resume")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+    const { user, loading } = useSession();
+    useEffect(() => {
+      if (!loading) {
+        if (!user) {
+          window.location.replace("/login");
+        } else if (user?.tierId === "starter") {
+          window.location.replace("/pricing");
+        }
+      }
+    }, [loading, user]);
+  
+    // Always show a spinner while deciding/redirecting to avoid UI flash
+    if (loading) return <VerifyingAuth />;
+    if (!user || user?.tierId === "starter") return <VerifyingAuth />;
+
   return <Resume />;
 }
